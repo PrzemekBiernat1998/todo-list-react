21154e908c7a502d2f7dffd0273e00d5deb86fc7
PrzemekBiernat1998 <przemek.biernat98@gmail.com>
2023-01-10 18:39:41 +0100

    style added to buttons

--- src/Buttons/styled.js
+++ src/Buttons/styled.js
@@ -2,20 +2,20 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
+    justify-content: end;
     flex-wrap: wrap;
 `;
+
 export const Button =  styled.button`
     border: none;
     background-color: transparent;
     color: teal;
     margin: 0 0 0 20px;
     transition: color 0.5s;
-
     @media(max-width: 767px){
         flex-basis:  100%;
         margin: 10px;
       }
-
     &:hover{
         color: hsl(180, 94%, 25%);
     }
