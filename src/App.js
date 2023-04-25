7bdaa06fca3ce73b0d075ef1f1ca76d9ffcfc220
PrzemekBiernat1998 <przemek.biernat98@gmail.com>
2023-04-25 18:49:31 +0200

    added navigation to other component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import {Switch, Route, HashRouter, Redirect } from "react-router-dom"
+import { Switch, Route, HashRouter, Redirect } from "react-router-dom"
 import Author from "./features/author/Author";
 import TasksPage from "./features/tasks/index";
 import TaskPage from "./features/tasks/TaskPage";
-import {StyledLi, StyledNav, StyledNavLink } from "./features/styled";
+import Navigation from "./Navigation";
+import { toAuthor, toTask, toTasks } from "./routes";
 
 
 const App = () => (
     <HashRouter>
-        <StyledNav>
-            <ul>
-                <StyledLi>
-                    <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-                </StyledLi>
-                <StyledLi>
-                    <StyledNavLink to="/author">O Autorze</StyledNavLink>
-                </StyledLi>
-            </ul>
-            <Switch>
-                <Route path="/zadania/:id">
-                    <TaskPage />
-                </Route>
-                <Route path="/zadania">
-                    <TasksPage />
-                </Route>
-                <Route path="/author">
-                    <Author />
-                </Route>
-                <Route path="/">
-                    <Redirect to="/zadania" />
-                </Route>
-            </Switch>
-        </StyledNav>
+        <Navigation />
+        <Switch>
+            <Route path={toTask()}>
+                <TaskPage />
+            </Route>
+            <Route path={toTasks()}>
+                <TasksPage />
+            </Route>
+            <Route path={toAuthor()}>
+                <Author />
+            </Route>
+            <Route path="/">
+                <Redirect to={toTasks()} />
+            </Route>
+        </Switch>
     </HashRouter>
 );
 
